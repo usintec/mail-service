e7c4e0a415f1271f27973366d314be9b976f152e
mac <mac@macs-MacBook-Air.local>
2022-07-15 12:37:21 +0100

    send mail

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const sgMail = require('@sendgrid/mail');
 const express = require("express");
 const app = express();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,8 +15,28 @@ app.use((req, res, next) => {
     next();
 });
 
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+
 app.get('/', (req, res) => {
     res.status(200).send('Our api is working fine');
+});
+
+app.get('/send-mail', (req, res) => {
+    sgMail
+        .send(msg)
+        .then(() => {}, error => {
+             console.error(error);
+            if (error.response) {
+                console.error(error.response.body)
+            }
+        }
+    );
 });
 
 try{
